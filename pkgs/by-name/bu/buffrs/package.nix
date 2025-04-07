2b8190b04ad3c838a8d8982130779fcc943f235e
Danilo Bürger <danilo.buerger@helsing.ai>
2025-04-07 10:37:26 +0200

    buffrs: init at 0.9.0

---
+++ pkgs/by-name/bu/buffrs/package.nix
@@ -0,0 +1,23 @@
+{ lib, fetchFromGitHub, rustPlatform }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "buffrs";
+  version = "0.9.0";
+
+  src = fetchFromGitHub {
+    owner = "helsing-ai";
+    repo = pname;
+    rev = "v${version}";
+    hash = "sha256-c9GjSqVp2wEFgoy8j+Gy5FA3SG4JYEfeSwPWjW81w3Y=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-E7kskULt2eOY+mZjh6jAftj8ciExUF7d1z1pePTBzvQ=";
+
+  meta = {
+    description = "Modern protobuf package management";
+    homepage = "https://github.com/helsing-ai/buffrs";
+    license = lib.licenses.asl20;
+    maintainers = with lib.maintainers; [ danilobuerger ];
+  };
+}
